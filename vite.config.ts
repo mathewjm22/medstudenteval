84023a018a2634672d4017b280cbf760c55cb8e3
Jacob Mathew <75663388+mathewjm22@users.noreply.github.com>
2026-02-15 09:31:16 -0700

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures assets load correctly on GitHub Pages subfolders
+  base: './', 
   define: {
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
   },
   build: {
     outDir: 'dist',
+    emptyOutDir: true
   }
 });
